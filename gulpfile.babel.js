bdcd1f8c327a84f88fbca6c25440aa4064b5677c
Alex Negodiaev <alex.negodiaev@gmail.com>
2018-04-11 15:09:56 +0500

    Deployed the project on Github Pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,23 +1,23 @@
 'use strict';
 
-import plugins       from 'gulp-load-plugins';
-import yargs         from 'yargs';
-import browser       from 'browser-sync';
-import gulp          from 'gulp';
-import panini        from 'panini';
-import rimraf        from 'rimraf';
-import sherpa        from 'style-sherpa';
-import yaml          from 'js-yaml';
-import fs            from 'fs';
+import plugins from 'gulp-load-plugins';
+import yargs from 'yargs';
+import browser from 'browser-sync';
+import gulp from 'gulp';
+import panini from 'panini';
+import rimraf from 'rimraf';
+import sherpa from 'style-sherpa';
+import yaml from 'js-yaml';
+import fs from 'fs';
 import webpackStream from 'webpack-stream';
-import webpack2      from 'webpack';
-import named         from 'vinyl-named';
+import webpack2 from 'webpack';
+import named from 'vinyl-named';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
 // Check for --production flag
-const PRODUCTION = !!(yargs.argv.production);
+const PRODUCTION = !!yargs.argv.production;
 
 // Load settings from settings.yml
 const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
@@ -28,12 +28,13 @@ function loadConfig() {
 }
 
 // Build the "dist" folder by running all of the below tasks
-gulp.task('build',
- gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy), styleGuide));
+gulp.task('build', gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy), styleGuide));
+
+// Turn the "dist" folder into the "docs" folder for Github Pages
+gulp.task('deploy', gulp.series(cleanDeploy, 'build', deploy));
 
 // Build the site, run the server, and watch for file changes
-gulp.task('default',
-  gulp.series('build', server, watch));
+gulp.task('default', gulp.series('build', server, watch));
 
 // Delete the "dist" folder
 // This happens every time a build starts
@@ -41,23 +42,36 @@ function clean(done) {
   rimraf(PATHS.dist, done);
 }
 
+// Delete the "docs" folder
+// This happens every time a deploy starts
+function cleanDeploy(done) {
+  rimraf(PATHS.deploy, done);
+}
+
 // Copy files out of the assets folder
 // This task skips over the "img", "js", and "scss" folders, which are parsed separately
 function copy() {
-  return gulp.src(PATHS.assets)
-    .pipe(gulp.dest(PATHS.dist + '/assets'));
+  return gulp.src(PATHS.assets).pipe(gulp.dest(PATHS.dist + '/assets'));
+}
+
+// Copy files out of the dist folder
+function deploy() {
+  return gulp.src(PATHS.dist + '/**/*').pipe(gulp.dest(PATHS.deploy));
 }
 
 // Copy page templates into finished HTML files
 function pages() {
-  return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
-    .pipe(panini({
-      root: 'src/pages/',
-      layouts: 'src/layouts/',
-      partials: 'src/partials/',
-      data: 'src/data/',
-      helpers: 'src/helpers/'
-    }))
+  return gulp
+    .src('src/pages/**/*.{html,hbs,handlebars}')
+    .pipe(
+      panini({
+        root: 'src/pages/',
+        layouts: 'src/layouts/',
+        partials: 'src/partials/',
+        data: 'src/data/',
+        helpers: 'src/helpers/'
+      })
+    )
     .pipe(gulp.dest(PATHS.dist));
 }
 
@@ -69,30 +83,40 @@ function resetPages(done) {
 
 // Generate a style guide from the Markdown content and HTML template in styleguide/
 function styleGuide(done) {
-  sherpa('src/styleguide/index.md', {
-    output: PATHS.dist + '/styleguide.html',
-    template: 'src/styleguide/template.html'
-  }, done);
+  sherpa(
+    'src/styleguide/index.md',
+    {
+      output: PATHS.dist + '/styleguide.html',
+      template: 'src/styleguide/template.html'
+    },
+    done
+  );
 }
 
 // Compile Sass into CSS
 // In production, the CSS is compressed
 function sass() {
-  return gulp.src('src/assets/scss/app.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      includePaths: PATHS.sass
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: COMPATIBILITY
-    }))
-    // Comment in the pipe below to run UnCSS in production
-    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
-    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/css'))
-    .pipe(browser.reload({ stream: true }));
+  return (
+    gulp
+      .src('src/assets/scss/app.scss')
+      .pipe($.sourcemaps.init())
+      .pipe(
+        $.sass({
+          includePaths: PATHS.sass
+        }).on('error', $.sass.logError)
+      )
+      .pipe(
+        $.autoprefixer({
+          browsers: COMPATIBILITY
+        })
+      )
+      // Comment in the pipe below to run UnCSS in production
+      //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+      .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
+      .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+      .pipe(gulp.dest(PATHS.dist + '/assets/css'))
+      .pipe(browser.reload({ stream: true }))
+  );
 }
 
 let webpackConfig = {
@@ -108,17 +132,23 @@ let webpackConfig = {
       }
     ]
   }
-}
+};
 // Combine JavaScript into one file
 // In production, the file is minified
 function javascript() {
-  return gulp.src(PATHS.entries)
+  return gulp
+    .src(PATHS.entries)
     .pipe(named())
     .pipe($.sourcemaps.init())
     .pipe(webpackStream(webpackConfig, webpack2))
-    .pipe($.if(PRODUCTION, $.uglify()
-      .on('error', e => { console.log(e); })
-    ))
+    .pipe(
+      $.if(
+        PRODUCTION,
+        $.uglify().on('error', e => {
+          console.log(e);
+        })
+      )
+    )
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.dist + '/assets/js'));
 }
@@ -126,17 +156,24 @@ function javascript() {
 // Copy images to the "dist" folder
 // In production, the images are compressed
 function images() {
-  return gulp.src('src/assets/img/**/*')
-    .pipe($.if(PRODUCTION, $.imagemin({
-      progressive: true
-    })))
+  return gulp
+    .src('src/assets/img/**/*')
+    .pipe(
+      $.if(
+        PRODUCTION,
+        $.imagemin({
+          progressive: true
+        })
+      )
+    )
     .pipe(gulp.dest(PATHS.dist + '/assets/img'));
 }
 
 // Start a server with BrowserSync to preview the site in
 function server(done) {
   browser.init({
-    server: PATHS.dist, port: PORT
+    server: PATHS.dist,
+    port: PORT
   });
   done();
 }
