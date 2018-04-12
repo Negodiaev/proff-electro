5435e1a7a758bf3a67092aed4014f64ad39afd64
Alex Negodiaev <alex.negodiaev@gmail.com>
2018-04-12 13:57:45 +0500

    Added a header and a banners section

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,24 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import whatInput from 'what-input';
 
-window.$ = $;
+// window.$ = $;
 
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
+import Swiper from 'swiper';
 
+// $(document).foundation();
 
-$(document).foundation();
+document.addEventListener('DOMContentLoaded', function(event) {
+  //------------------------------------------------------------------------//
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination'
+    }
+  });
+  //------------------------------------------------------------------------//
+});
